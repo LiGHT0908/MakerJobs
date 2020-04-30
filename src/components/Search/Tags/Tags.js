ce5d1e7cf07cc999b6c0bf0908bca615236da1a5
Mayank Kulkarni <mayank0kulkarni@gmail.com>
2020-04-30 16:53:26 +0530

    added tagcolours to state

--- src/components/Search/Tags/Tags.js
+++ src/components/Search/Tags/Tags.js
@@ -4,15 +4,25 @@ import './Tags.css'
 
 class Tags extends React.Component{
     
-    constructor(props){
+    constructor (props)
+    {
         super(props);
         this.state = {
             tags : [
                 "php", "Java", "Javascript", "Photoshop", "Primere", "Python", "C", "C++", "C#","ReactJS", "AngularJS", "NodeJS", "HTML", "CSS", "Bootstrap", "php", "Numpy", "Ruby", "scss", "nodemodules" 
             ],
             colours: ["#d15353","#a7e248","#3e8ae2","#69e265","#ee9b4d"],
-            width: window.innerWidth
+            width: window.innerWidth,
+            colortags:[]
         }
+
+        for (let i=0;i<this.state.tags.length;i++)
+        {   
+            let random=this.state.colours[Math.floor(Math.random() * this.state.colours.length)]
+            this.state.colortags.push(random)
+        }
+        
+
         if (this.state.width<=577)
         {
             var reducedTags = [...this.state.tags];
@@ -21,15 +31,14 @@ class Tags extends React.Component{
         } 
     }
     render = () =>
-    {   
+    {    var randomcolor = Math.floor( Math.random() * this.state.colours.length )
         
         return (
             <div className="tags-container">
-              {this.state.tags.map( item => {
-                  var randomcolor = Math.floor( Math.random() * this.state.colours.length )
+              {this.state.tags.map( (item , index) => {
                   return (
                     <div className="tag-packet">
-                      <p><span style={{background:this.state.colours[randomcolor]}}>#</span> {item}</p>
+                      <p><span style={{background:this.state.colortags[index]}}>#</span> {item}</p>
                     </div>
                   )
               })}
